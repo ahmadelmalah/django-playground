20fba7eac051369355627928d956a9f827108b30
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-04-16 02:13:17 +0200

    Listing members

--- members/views.py
+++ members/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    members = Member.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'members': members
+    }
+    return HttpResponse(template.render(context, request))
