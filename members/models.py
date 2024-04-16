735280f8c50d5c4a75cea1ad9291a689b0aafaed
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-04-16 02:45:44 +0200

    Admin

--- members/models.py
+++ members/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.firstname + ' ' + self.lastname
